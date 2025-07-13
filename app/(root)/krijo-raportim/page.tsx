ebaff4a1b47f3e0e1c817f7eef0622dadb9785c8
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-07-14 01:49:04 +0200

    add audis and videos attached options to complains

--- app/(root)/krijo-raportim/page.tsx
+++ app/(root)/krijo-raportim/page.tsx
@@ -1,8 +1,14 @@
 import CreateComplaintForm from '@/components/CreateComplaintForm'
 import Link from 'next/link'
 import React from 'react'
+import { getServerSession } from 'next-auth'
+import { redirect } from 'next/navigation'
 
-const page = () => {
+const page = async () => {
+  const session = await getServerSession()
+  if(!session){
+    redirect("/kycuni")
+  }
   return (
     <>
     <div className="w-full max-w-6xl mx-auto py-10 px-4 sm:px-6 lg:px-8 text-center shadow-xl">
