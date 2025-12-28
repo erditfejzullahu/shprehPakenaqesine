c66bda74ef744155c111265f0fa3ef5d3495cc10
Erdit Fejzullahu <erditfejzullahu45@gmail.com>
2025-12-28 17:01:40 +0000

    fixes

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "shfaqpakenaqesine",
+      script: "npm",
+      args: "run start",
+      env: {
+        NODE_ENV: "production",
+PORT:4005
+      }
+    }
+  ]
+};
